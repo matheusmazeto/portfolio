29c315c91068c3625d07097f8bcf8f305b3fbd63
matheusmazeto <mgmazeto@gmail.com>
2024-01-22 16:08:58 +0000

    feat: add dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import ThemeSwitch from './components/theme-switch'
+
 export default function Home() {
-  return <h1>Hello World!</h1>
+  return (
+    <h1>
+      Hello World!
+      <ThemeSwitch />
+    </h1>
+  )
 }
